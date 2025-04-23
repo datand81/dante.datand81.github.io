7c9de64d7413cd31f2248b73789cc17f1a459a3f
datand81 <dativo.andres@gmail.com>
2025-04-24 01:27:34 +0200

    Update astro.config.mjs
    Signed-off-by: datand81 <dativo.andres@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: portfolio.github.io,
+    site: website,
     vite: {
         plugins: [tailwindcss()]
     },
