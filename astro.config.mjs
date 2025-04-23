338ee306c5f5591095da7d274b4d0fcb092c0a36
datand81 <dativo.andres@gmail.com>
2025-04-23 23:53:26 +0200

    add deploy configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.portfolio.github.io,
+    site: portfolio.github.io,
     vite: {
         plugins: [tailwindcss()]
     },
